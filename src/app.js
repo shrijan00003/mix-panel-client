adb16921350c0e204fc20a7e0c61d6c1407dae78
shrijan <shrijan00003@gmail.com>
2018-08-19 23:28:19 +0545

    API CALL MODIFIED

--- src/app.js
+++ src/app.js
@@ -1,60 +1,67 @@
 import * as MIXPANEL from "./services/mixPanelServices";
 
 /**
- * MIXPANEL.identify() function to identifiy for the very first time 
+ * MIXPANEL.configure() function to identifiy for the very first time
+ *
  */
-const identify = () =>{
-  MIXPANEL.identify({
-    clientId : 'abc123',
-    email : 'shrijan00003@gmail.com'
+
+const configure = () => {
+  MIXPANEL.configure({
+    apiKey: "abc123",
+    email: "shrijan00003@gmail.com"
   })
-  .then(data => console.log(data))
-  .catch(err => console.error(err));
-}
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+};
 
-
+const identify = () => {
+  MIXPANEL.identify({
+    userId: "1111111",
+    userEmail: "st@gmail.com",
+    userName: "st",
+    userDetails: {
+      name: "shrijan triapthi",
+      addrss: "kapan"
+    }
+  })
+    .then(data => console.log(data))
+    .catch(err => console.error(err));
+};
 
 /**
- * 
+ *
  */
 
 const track = () => {
   MIXPANEL.track({
-    plan:"pro",
-    name:"Delete the facebook Account",
-    eventName : "delete_account",
-    payload:{
+    name: "Delete the facebook Account",
+    eventName: "delete_account",
+    payload: {
       user: "shrijan sharma",
       change: "Account Delete",
       reaseon: "feels insecure"
     }
   })
-  .then(data => console.log(data))
-  .catch(err => console.error(err));
-}
+    .then(data => console.log(data))
+    .catch(err => console.error(err));
+};
 
 /**
- * 
+ *
  */
 const page = () => {
   MIXPANEL.page({
-    name: 'Home',
-    title : 'Home',
-    keywords : ['home', 'news', 'top', 'hello']
+    name: "Home",
+    title: "Home",
+    keywords: ["home", "news", "top", "hello"]
   })
-  .then(data => console.log(data))
-  .catch(err => console.log(err))
-}
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+};
 
-
-// identify();
+configure();
+identify();
 // track();
-// page();
+page();
 
-
-
-
-
-
-
-
+// MIXPANEL.getallMetadata();
--- src/services/mixPanelServices.js
+++ src/services/mixPanelServices.js
@@ -1,132 +1,267 @@
 import http from "../utils/http";
 import { getUserDetails, getLocation } from "../utils/userDetails";
 import { getFirstWord, getLastWord } from "../utils/lodash";
+import UID from "uuid/v1";
 
-export async function identify(params) {
-    try {
-        const { clientId, email } = params;
-        
-        const savedData = await saveInLocalStorage( clientId, email);
-        if ( savedData ) {
-            const response = await http.post(`mixpanel/identify`)
+let isNewUser = false;
+let metadataId = undefined;
 
-            if ( response.status === 200 ) {
-                return response;
-            }
-        }
+export async function configure(params) {
+  try {
+    const { apiKey, email } = params;
 
-    } catch (err) {
-        console.error(err);
+    const savedData = await saveInLocalStorage(apiKey, email);
+
+    if (savedData) {
+      return true;
     }
-
+  } catch (err) {
+    console.error(err);
+  }
 }
 
-export async function track(params) {
-    try {
-    
-        const { plan, name, eventName , payload } = params;
+export async function identify(params = {}) {
+  try {
+    const { userId, userEmail, userName, userDetails, ...rest } = params;
 
-        const trackData = {
-            plan,
-            name,
-            eventName,
-            payload
+    if (userId) {
+      const userInfo = {
+        userId,
+        userName,
+        userEmail,
+        userDetails
+      };
+
+      // saving in localstorage
+      const data = JSON.stringify(userInfo);
+      await localStorage.setItem("userInfo", data);
+    } else {
+      // create userid and other info
+
+      const userInfo = {
+        userId: UID(),
+        userName: "test",
+        userDetails: null,
+        userEmail: "test@mail.com"
+      };
+
+      //save in local storage
+      const data = JSON.stringify(userInfo);
+      await localStorage.setItem("userInfo", data);
+    }
+
+    const deviceMetaData = await getAllMetadata();
+    const isConfigured = await checkIfConfigured();
+    const metadataId = await localStorage.getItem("metadataId");
+    const userInfo = await JSON.parse(localStorage.getItem("userInfo"));
+
+    if (metadataId === null && isConfigured) {
+      const metaData = {
+        ...userInfo,
+        ...deviceMetaData
+      };
+
+      const response = await http.post(`mixpanel/identify`, {
+        data: {
+          metaData
         }
+      });
 
+      if (response.status === 200) {
+        await localStorage.setItem("metadataId", response.data.id);
 
-        const metaData = await getallMetadata();
+        return response;
+      }
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
 
-        console.log( trackData , metaData );
+const checkIfConfigured = () => {
+  const clientId = localStorage.getItem("clientId");
+  const email = localStorage.getItem("email");
 
-        const data = {
-            trackData,
-            metaData
+  return clientId && email ? true : false;
+};
+
+export async function track(params = {}) {
+  try {
+    const { name, eventName, payload } = params;
+
+    const deviceMetaData = await getAllMetadata();
+    const isConfigured = await checkIfConfigured();
+    const metadataId = await localStorage.getItem("metadataId");
+    const userInfo = await JSON.parse(localStorage.getItem("userInfo"));
+
+    if (userInfo === null) {
+      return;
+    }
+
+    const trackData = {
+      name,
+      eventName,
+      payload
+    };
+
+    const getMetaData = () => {
+      return metadataId
+        ? { metadataId }
+        : {
+            ...userInfo,
+            ...deviceMetaData
+          };
+    };
+
+    const metaData = await getMetaData();
+    if (isConfigured) {
+      const res = await http.post(`mixpanel/track`, {
+        data: {
+          trackData,
+          metaData
         }
+      });
 
-        const res = await http.post(`mixpanel/track`, {
-            data : {
-                trackData,
-                metaData,
-            }
-        });
-
+      if (res.status === 200) {
         return res;
-
-    } catch ( err ) {
-        console.error( err );
+      }
     }
+  } catch (err) {
+    console.error(err);
+  }
 }
-    
-export async function page ( params ) {
-    try {
-        const { name , title , keywords } = params;
-        
-        const pageInfo = await getPageInfo();
-        
-        const pageData = {
-            name,
-            path: pageInfo.path,
-            referrer: pageInfo.referrer,
-            search: pageInfo.search,
-            url: pageInfo.url,
-            title,
-            keywords
+
+export async function page(params) {
+  try {
+    const { name, title, keywords } = params;
+
+    const deviceMetaData = await getAllMetadata();
+    const isConfigured = await checkIfConfigured();
+    const metadataId = await localStorage.getItem("metadataId");
+    const userInfo = await JSON.parse(localStorage.getItem("userInfo"));
+
+    if (userInfo === null) {
+      return;
+    }
+    const pageInfo = await getPageInfo();
+
+    const getMetaData = () => {
+      return metadataId
+        ? { metadataId }
+        : {
+            ...userInfo,
+            ...deviceMetaData
+          };
+    };
+
+    const metaData = await getMetaData();
+
+    const pageData = {
+      name,
+      path: pageInfo.path,
+      referrer: pageInfo.referrer,
+      search: pageInfo.search,
+      url: pageInfo.url,
+      title,
+      keywords
+    };
+
+    const allMetaData = {
+      ...userInfo,
+      ...deviceMetaData
+    };
+
+    if (isConfigured) {
+      const res = await http.post(`mixpanel/page`, {
+        data: {
+          pageData,
+          metaData
         }
-        
-        const metaData = await getallMetadata();
-     
-        const res = await http.post(`mixpanel/page`, {
-            data : {
-                pageData,
-                metaData,
-            }
-        });
-
-        return res;
-        
-    } catch ( err ) {
-            console.log ( err );
-        }
-        
-} // end of page funciton 
-
-async function getPageInfo () {
-    const referrer = await document.referrer;
-    const path = await window.location.pathname;
-    console.log('path', path);
-    const url = await window.location.href;
-    const search = await getLastWord(path);
-    return {
-        referrer,
-        path,
-        url,
-        search
+      });
+      return res;
     }
+  } catch (err) {
+    console.log(err);
+  }
+} // end of page funciton
+
+async function getPageInfo() {
+  const referrer = await document.referrer;
+  const path = await window.location.pathname;
+  const url = await window.location.href;
+  const search = await getLastWord(path);
+  return {
+    referrer,
+    path,
+    url,
+    search
+  };
 }
 
-async function saveInLocalStorage ( clientId, email ){
-    try {
-        await localStorage.setItem( 'clientId', clientId );
-        await localStorage.setItem( 'email', email );
+async function saveInLocalStorage(apiKey, email) {
+  try {
+    await localStorage.setItem("clientId", apiKey);
+    await localStorage.setItem("email", email);
 
-        return true;
-    } catch (err) {
-        console.err(err);
-    }
+    return true;
+  } catch (err) {
+    console.err(err);
+  }
 }
 
-async function getallMetadata () {
-    const allMetaData = await getUserDetails();
-    const currnetLocation = await getLocation();
+export async function getAllMetadata() {
+  const allMetaData = await getUserDetails();
+  const currnetLocation = await getLocation();
 
-    const device = allMetaData.screenSize;
-    const os = await getFirstWord(allMetaData.os);
-    const browser = await getFirstWord(allMetaData.browser);
+  const device = allMetaData.screenSize;
+  const os = await getFirstWord(allMetaData.os);
+  const browser = await getFirstWord(allMetaData.browser);
 
-    return {
-        browser,
-        os,
-        device,
-        location : currnetLocation
-    }
+  return {
+    os,
+    device,
+    browser,
+    location: currnetLocation
+  };
 }
+
+const getUserInfo = async () => {
+  if (localStorage.getItem("userId")) {
+    const userId = await localStorage.getItem("userId");
+    const userEmail = await localStorage.getItem("userEmail");
+    const metadataId = await localStorage.getItem("metaDataId");
+    isNewUser = metadataId ? false : true;
+    return { userId, metadataId, userEmail, isNewUser };
+  } else {
+    const id = UID();
+    isNewUser = true;
+    const userId = await localStorage.setItem("userId", id);
+    const userEmail = await localStorage.setItem(
+      "userEmail",
+      "anonymous@mail.com"
+    );
+
+    return { userId, userEmail, isNewUser };
+  }
+};
+
+const setUserInfo = async userParams => {
+  if (userParams) {
+    await localStorage.setItem("userId", userParams.userId);
+    await localStorage.setItem("userEmail", userParams.userEmail);
+
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const getUserId = () =>
+  localStorage.getItem("mixPanelUserId")
+    ? localStorage.getItem("mixPanelUserId")
+    : null;
+
+const getUserEmail = () =>
+  localStorage.getItem("mixPanelUserEmail")
+    ? localStorage.getItem("mixPanelUserEmail")
+    : null;
