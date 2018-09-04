e1d1163f25e8dfa172fa63f008eaf0abd51e492a
shrijan <shrijan00003@gmail.com>
2018-09-04 14:17:48 +0545

    v0.1.3

--- src/app.js
+++ src/app.js
@@ -1,28 +1,22 @@
 import * as MIXPANEL from "./services/mixPanelServices";
 
-// window.MIXPANEL = MIXPANEL;
+(function() {
+  // Establish the root object, `window` in the browser, or `global` on the server.
+  let root = this;
 
-module.exports.default = MIXPANEL;
+  // Create a reference to this
+  let MIXPANEL = new Object();
 
-// (function() {
-//   // Establish the root object, `window` in the browser, or `global` on the server.
-//   var root = this;
+  let isNode = false;
 
-//   // Create a reference to this
-//   var _ = new Object();
-
-//   var isNode = false;
-
-//   // Export the Underscore object for **CommonJS**, with backwards-compatibility
-//   // for the old `require()` API. If we're not in CommonJS, add `_` to the
-//   // global object.
-//   if (typeof module !== "undefined" && module.exports) {
-//     module.exports = _;
-//     root._ = _;
-//     isNode = true;
-//   } else {
-//     root._ = _;
-//   }
-// })();
-
-// export default MIXPANEL;
+  // Export the Underscore object for **CommonJS**, with backwards-compatibility
+  // for the old `require()` API. If we're not in CommonJS, add `_` to the
+  // global object.
+  if (typeof module !== "undefined" && module.exports) {
+    module.exports = MIXPANEL;
+    root.MIXPANEL = MIXPANEL;
+    isNode = true;
+  } else {
+    root.MIXPANEL = MIXPANEL_;
+  }
+})();
